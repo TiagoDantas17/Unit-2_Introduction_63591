09fc776bbc67a861f200aca70d2e26ddd028d6f2
a63591_TiagoDantas <tmgd2006@gmail.com>
2026-03-10 12:41:18 +0000

    Launch projectile on spacebar press

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+
+    public GameObject projectilePrefab;
     void Start()
     {
         
@@ -22,5 +24,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
