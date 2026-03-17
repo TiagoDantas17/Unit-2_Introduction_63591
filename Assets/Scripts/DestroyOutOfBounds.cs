0dd683d099449cae20994ec18854b9cb4bc8d1db
TiagoDantas17 <tmgd2006@gmail.com>
2026-03-17 15:20:52 +0000

    Tutorial complete!

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -7,22 +7,24 @@ public class Dest : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
-  {
-   
-    if (transform.position.z > topBound)
-   
     {
-        Destroy(gameObject); 
+        // If an object goes past the players view in the game, remove that object
+        if (transform.position.z > topBound)
 
-    } else if (transform.position.z < lowerBound)
         {
+            Destroy(gameObject);
+
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
 
-    } 
-} 
+    }
+}
